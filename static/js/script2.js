8170ef622a7a674e027b452ddff575f98e655ad0
Anmol aashish <aabatwal20@gmail.com>
2024-04-12 21:18:15 +0530

    Made some changes in the design

--- static/js/script2.js
+++ static/js/script2.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", async function () {
   const inputMessage = document.getElementById("inputMessage");
-  const sendButton = document.getElementById("send");
+  const sendButton = document.querySelector(".send");
   const display = document.getElementById("display");
   let sendingMessage = false;
 
@@ -9,6 +9,10 @@ document.addEventListener("DOMContentLoaded", async function () {
       const message = inputMessage.value.trim();
       if (message !== "") {
         sendingMessage = true;
+        sendButton.removeAttribute("id");
+        sendButton.textContent = "";
+        sendButton.classList.add("loader");
+
         const userInputElement = document.createElement("div");
         userInputElement.classList.add("user-message");
         userInputElement.innerHTML = `<strong>You:</strong> ${message}`;
@@ -21,6 +25,9 @@ document.addEventListener("DOMContentLoaded", async function () {
         display.appendChild(botOutputElement);
         display.scrollTop = display.scrollHeight;
         sendingMessage = false;
+        sendButton.classList.remove("loader");
+        sendButton.textContent = "Send";
+        sendButton.setAttribute("id", "send");
       }
     }
   }
